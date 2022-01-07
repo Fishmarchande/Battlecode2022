74cffd215a20a508dd9b55755dec227b3e97253f
Fishmarchande <masonneon@gmail.com>
2022-01-06 22:06:23 -0500

    Basic Miner
    randomly wanders and mines

--- src/player/Miner.java
+++ src/player/Miner.java
@@ -3,26 +3,51 @@ import battlecode.common.*;
 
 
 public class Miner extends RobotPlayer{
-    static MapLocation orgin;
+    static MapLocation origin;
     static MapLocation leadSource = null;
     static MapLocation[]leadSources = null;
     static boolean foundLead = false;
+    static boolean miningLead = false;
     public static void run(RobotController rc) throws GameActionException {
 
-        if(!foundLead){
-            leadSources = rc.senseNearbyLocationsWithLead(20);
+        if(turnCount == 1){
+            origin = rc.getLocation();
+        }
+
+        for (Direction dir:directions){
+            if(rc.canMineLead(rc.adjacentLocation(dir))){
+                rc.mineLead(rc.adjacentLocation(dir));
+            }
+        }
+
+
+
+
+        leadSources = rc.senseNearbyLocationsWithLead(20); // 100 bytecode, not that bad
+        if(leadSources.length > 0){ // if there is a place for us to mine, then lets try to go to the first one
             foundLead = true;
             leadSource = leadSources[0];
         }
-        //if(LeadSources.size < 0){
-         //   foundLead = false;
-        //}
-
-        while (rc.canMineLead(leadSource)) {
-            rc.mineLead(leadSource);
-            System.out.println("MINE");
+        if(foundLead){
+            if(rc.canMove(rc.getLocation().directionTo(leadSource))) {
+                rc.move(rc.getLocation().directionTo(leadSource));
+            }
+        }
+        if(!foundLead){// looking for lead to mine
+            Direction dir = directions[rng.nextInt(directions.length)];
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+                System.out.println("I moved!");
+            }
         }
 
-        rc.move(rc.getLocation().directionTo(leadSource));
-        }// move towards known leadsource
+
+
+
+
+
+
+
+
+    }
 }
