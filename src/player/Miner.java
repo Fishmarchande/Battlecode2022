c28c4de01998fdba99a478a31b7fc4883d7185d0
Thomas <19950277+CaptThomas@users.noreply.github.com>
2022-01-07 20:23:12 -0500

    Update Miner.java
    - check for new lead sources only each 4 turns
    - goes in a straight line and keeps it
    - handles walls

--- src/player/Miner.java
+++ src/player/Miner.java
@@ -7,11 +7,13 @@ public class Miner extends RobotPlayer{
     static MapLocation leadSource = null;
     static MapLocation[]leadSources = null;
     static boolean foundLead = false;
-    static boolean miningLead = false;
+    //static boolean miningLead = false; // UNUSED
+    static Direction oneLine = null;
     public static void run(RobotController rc) throws GameActionException {
 
         if(turnCount == 1){
             origin = rc.getLocation();
+            oneLine = directions[rng.nextInt(directions.length)];
         }
 
         for (Direction dir:directions){
@@ -22,11 +24,14 @@ public class Miner extends RobotPlayer{
 
 
 
-
-        leadSources = rc.senseNearbyLocationsWithLead(20); // 100 bytecode, not that bad
+        if(turnCount % 4 == 0) { // Check for lead sources (within sense range of 20 radii squared) every 4 turns and put them in the array leadSources
+            leadSources = rc.senseNearbyLocationsWithLead(20); // 100 bytecode, not that bad
+        }
         if(leadSources.length > 0){ // if there is a place for us to mine, then lets try to go to the first one
             foundLead = true;
             leadSource = leadSources[0];
+        } else {
+            foundLead = false;
         }
         if(foundLead){
             if(rc.canMove(rc.getLocation().directionTo(leadSource))) {
@@ -34,10 +39,14 @@ public class Miner extends RobotPlayer{
             }
         }
         if(!foundLead){// looking for lead to mine
-            Direction dir = directions[rng.nextInt(directions.length)];
-            if (rc.canMove(dir)) {
-                rc.move(dir);
+            Direction dir2 = directions[rng.nextInt(directions.length)];
+            if (rc.canMove(oneLine)) { // Move in a straight line determined earlier
+                rc.move(oneLine);
                 System.out.println("I moved!");
+            } else if (rc.canMove(dir2)) { // Most likely hit a wall, set new oneLine and move there
+                rc.move(dir2);
+                oneLine = dir2;
+                System.out.println("I moved in the backup location");
             }
         }
 
